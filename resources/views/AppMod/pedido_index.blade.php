65c70fa65e0cfe61282fec103cf15282c5b50866
Tian <cris.tian.ct545@gmail.com>
2022-09-26 19:52:36 -0400

    cliente1.1

---
+++ resources/views/AppMod/pedido_index.blade.php
@@ -0,0 +1,17 @@
+<div class="bg-body-light">
+    <div class="content content-full">
+        <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+            <h1 class="flex-sm-fill h3 my-2">
+                Registrar Cliente <small
+                    class="d-block d-sm-inline-block mt-2 mt-sm-0 font-size-base font-w400 text-muted">Complete los datos requeridos.</small>
+            </h1>
+        </div>
+    </div>
+</div>
+<!-- END Hero -->
+
+<!-- Page Content -->
+<div class="content">
+  
+</div>
+<!-- END Page Content -->
